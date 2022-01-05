d3e0bd593a115a041964ad6804bd498f79febc30
Reem AlRashed <reemalrashed2@gmail.com>
2022-01-05 09:14:19 +0300

    Changed favicon

--- forms/contact.php
+++ forms/contact.php
@@ -11,7 +11,7 @@
 
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+    include('../assets/vendor/php-email-form/php-email-form.php');
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
@@ -26,12 +26,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
